bd948adde9822c915bd4350503f33a6872737bb8
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-05-12 22:42:47 +0100

    Implement Error handler and fetch order data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu, {loader as menuLoader} from "./features/menu/Menu";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+
     children: [
       {
         path: "/",
@@ -17,7 +19,8 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
-        loader: menuLoader
+        loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -30,6 +33,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
