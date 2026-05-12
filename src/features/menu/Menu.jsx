0ff590371526d43d15f18c500c578981dbb51109
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-05-12 22:01:17 +0100

    Implement data fetching using react router loader capability

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,23 @@
-function Menu() {
-  return <h1>Menu</h1>;
-}
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
+function Menu() {
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <li key={pizza.id}>
+          <MenuItem pizza={pizza} key={pizza.id}/>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export async function loader() {
+  const menu = await getMenu();
+
+  return menu;
+}
 export default Menu;
