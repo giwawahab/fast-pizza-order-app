e8591424360cac6d1d018a7cff570bf52fb30529
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-05-16 12:16:07 +0100

    implement simple login and model the cart reducer

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -37,7 +38,7 @@ function CreateOrder() {
   const isSubmitting = navigation.state === "submitting";
 
   const formErrors = useActionData();
-
+  const username = useSelector((state) => state.user.username);
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -49,7 +50,7 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col sm:flex-row gap-2 sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input className="input grow" type="text" defaultValue={username} name="customer" required />
         </div>
 
         <div className="mb-5 flex flex-col sm:flex-row gap-2 sm:items-center">
